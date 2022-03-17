ec905051722ec0dd7126cda8662296f0227335c2
Klaim (A. Joël Lamotte) <mjklaim@gmail.com>
2022-03-17 15:48:39 +0100

    first commit

---
+++ hello/hello/hello.cxx
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <libhello/hello.hxx>
+
+int main (int argc, char* argv[])
+{
+  using namespace std;
+
+  if (argc < 2)
+  {
+    cerr << "error: missing name" << endl;
+    return 1;
+  }
+
+  hello::say_hello(cout, argv[1]);
+}
